0d9cc0af513d615a9689ae0b140249b457c657de
Franco <francoRS832@gmail.com>
2024-10-16 11:42:53 -0500

    Corregido método de autenticación y configuraciones del controlador

--- database/migrations/2024_10_10_151514_create_articulos_servicios_table.php
+++ database/migrations/2024_10_10_151514_create_articulos_servicios_table.php
@@ -13,14 +13,23 @@ return new class extends Migration
     {
         Schema::create('articulos_servicios', function (Blueprint $table) {
             $table->id();
-            $table->string('item_name', 150);  
+            $table->string('item_name', 255) ->change();
             $table->text('description');  // TEXT
             $table->enum('content_type', ['object', 'service']);  
-            $table->string('file', 255);  
-            $table->string('service_url', 255);  
+            $table->string('file', 500) ->change();
+            $table->string('service_url', 500) ->change(); 
             $table->date('publish_date'); 
             $table->string('author', 100);  
-            $table->boolean('status');  
+            $table->integer('status')->default(1);
+            
+            // Campos que asocian el registro con el usuario y la entidad
+            $table->unsignedBigInteger('user_id');  // Relación con el usuario que sube el contenido
+            $table->unsignedBigInteger('entidad_id');  // Relación con la entidad que guarda el registro
+
+            // Definir las relaciones foráneas
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('entidad_id')->references('id')->on('entidades')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
@@ -28,8 +37,10 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
+
     public function down(): void
     {
         Schema::dropIfExists('articulos_servicios');
     }
+    
 };
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticuloServicioController;
+
+Route::post('/articulos', [ArticuloServicioController::class, 'store'])->name('articulos.store');
 
 Route::get('/', function () {
     return view('welcome');
