30a914e60a2d2d0044ffe9c51c29a11d42731dd5
Franco <francoRS832@gmail.com>
2024-10-31 13:23:11 -0500

    Modificaciones hechas

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,16 +15,16 @@ class AuthController extends Controller
 {
     public function login(Login $request)
     {
-        try{
+        try {
             $user = User::verifyCredentials($request->email, $request->password);
-            if(!is_object($user)){
-                switch($user){
+            if (!is_object($user)) {
+                switch ($user) {
                     case 1:
                         $message = 'Usuario inactivo';
-                    break;
+                        break;
                     case 2:
                         $message = 'Contraseña incorrecta';
-                    break;
+                        break;
                 }
                 return response()->json([
                     'message' => $message
@@ -38,28 +38,29 @@ class AuthController extends Controller
                 'tokenOperation' => $tokenOperation->generate($user),
                 'tokenUpdate' => $tokenUpdate->generate($user)
             ], 200);
-        }catch(Exception $e){
+        } catch (Exception $e) {
             Log::error(get_class($this) . 'method : ' .  __FUNCTION__ . ': ' . $e->getMessage());
             return response()->json([
-                'message'=>'Estamos experimentando problemas temporales',
+                'message' => 'Estamos experimentando problemas temporales',
+                $e->getMessage()
             ], 500);
         }
     }
 
     public function refreshTokens(Request $request)
     {
-        try{
+        try {
             $user = $request->user();
             list($tokenOperation, $tokenUpdate) = TokenService::refreshTokens($user);
             return response()->json([
                 'tokenOperation' => $tokenOperation,
                 'tokenUpdate' => $tokenUpdate
             ], 200);
-        }catch(Tokens $e){
+        } catch (Tokens $e) {
             return response()->json([
                 'message' => $e->getMessage()
             ], 401);
-        }catch(Exception $e){
+        } catch (Exception $e) {
             Log::error(get_class($this) . 'method : ' .  __FUNCTION__ . ': ' . $e->getMessage());
             return response()->json([
                 'message' => 'Estamos experimentando problemas'
@@ -69,13 +70,13 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        try{
+        try {
             $user = $request->user();
             $user->tokens()->delete();
             return response()->json([
                 'message' => 'clario'
             ], 200);
-        }catch(Exception $e){
+        } catch (Exception $e) {
             Log::error(get_class($this) . 'method :' . __FUNCTION__ . ':' . $e->getMessage());
             return response()->json([
                 'message' => 'Estamos experimentando problemas temporales'
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -50,5 +50,21 @@ class User extends Authenticatable
     public function entidad()
     {
         return $this->belongsTo(Entidad::class);
+    }
+
+    /**
+     *  VerifyStatus of user
+     * @return \App\Models\User if the credententials are validate or int (1, user disabled), (2, password wrong)
+     */
+    public static function verifyCredentials(string $email, string $password): \App\Models\User|int
+    {
+        $user = self::where('email', $email)->first();
+        if ($user->status == 0) {
+            return 1;
+        }
+        if (!Hash::check($password, $user->password)) {
+            return 2;
+        }
+        return $user;
     }
 }
